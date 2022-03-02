0cf0d4b15ef7747692d0039398da1074c3b73d99
= <=>
2022-03-02 18:25:38 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/lhdrones/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.lhdrones.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lhdrones.cursomc.domain.Categoria;
+import com.lhdrones.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) { 
+	 	Optional<Categoria> obj = repo.findById(id);  	
+	 	return obj.orElse(null); 
+	} 
+
+
+}
